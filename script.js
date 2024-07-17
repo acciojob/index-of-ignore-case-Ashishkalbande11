116a2f7bce7db126b217410196c6177ef9eca5b4
Ashishkalbande11 <154236313+Ashishkalbande11@users.noreply.github.com>
2024-07-17 17:23:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,11 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
-	const charSet = new Set();
-    for (let i = 0; i < s2.length; i++) {
-        charSet.add(s2.charAt(i).toLowerCase());
+	for(int i=0; i<s1.length(); i++){
+	    if(s1[i] == s2[0]){
+	        return i;
+	    }
     }
-
-    for (let i = 0; i < s1.length; i++) {
-        if (charSet.has(s1.charAt(i).toLowerCase())) {
-            return i + 1;
-        }
-    }
-
-    
-    return -1;
+return -1;
 }
 
 // Please do not change the code below
